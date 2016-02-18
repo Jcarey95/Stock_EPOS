afdcf424d00ad453a496fa9a625d7afb35676793
Jcarey95 <joshuacarey.jc@gmail.com>
2016-02-18 22:29:24 +0000

    Amendments

--- Main.py
+++ Main.py
@@ -1,19 +1,22 @@
-# Start The Log
 import logging
 from datetime import datetime
 from StartUp.Start_Up import first_run
-
+# Start The Log
 log_name = datetime.now().strftime('Logs/%d_%m_%Y_%H_%M.log')
 
 logging.basicConfig(filename=log_name, level=logging.DEBUG, format='%(asctime)s %(name)s ----%(message)s----',
                     datefmt='%m/%d/%Y %I:%M:%S %p')
 log = logging.getLogger(__name__)
 log.info('Log Started')
+
 # Run Start Up Script
 log.info('First Run Called')
+
 firstrun = first_run()
+
 log.info('First Run Successfully Run')
-if first_run() > 0:
+
+if firstrun > 0:
     log.info('Shut Down Program')
 
 
--- StartUp/Start_Up.py
+++ StartUp/Start_Up.py
@@ -1,6 +1,7 @@
 # Check Self Integrity
 import csv
 import logging
+import hashlib
 
 log = logging.getLogger(__name__)
 log.info('Start Up Script Begin')
@@ -8,11 +9,12 @@ log.info('Start Up Script Begin')
 
 def first_run():
     global hash_code, actual_hash_code, f_name
+
     checker = 0
+
     with open('StartUp/Files.csv', 'r') as f:
         hash_code_file = csv.reader(f)
         for row in hash_code_file:
-            import hashlib
             f_name = row[0]
             file_path = row[1]
             hash_code = row[2]
@@ -28,9 +30,6 @@ def first_run():
             log.info(f_name + ' Hash Failed')
             checker += 1
 
-    # Summary Of Program
-    if checker > 0:
-        log.info('Program Corrupt')
-
     log.info('Hash Check Complete')
+
     return checker
